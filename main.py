cf29592dd65f59e575720fc805129a276e10568c
Saif ul islam <sai5i.islam@gmail.com>
2023-02-14 04:22:32 +0000

    updated code for better readability

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-crossroad = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"").lower()
+crossroad = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\" ").lower()
 
 if crossroad == 'left':
-    waitorswim = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.").lower()
+    waitorswim = input("You've come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across. ").lower()
   
     if waitorswim == 'wait':
-      doorcolor = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
+      doorcolor = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()
       if doorcolor == 'blue':
         print("You are eaten by beasts. Game Over")
     
